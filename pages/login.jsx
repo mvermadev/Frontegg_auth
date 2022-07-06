8241a396671cd38925e09d1d0e1a59e54880b08d
intelloger-mverma <manish.verma@intelloger.com>
2022-07-06 15:24:11 +0530

    mnr

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,5 +1,6 @@
 import { useAuth, useLoginWithRedirect, AdminPortal } from '@frontegg/nextjs';
 import { ContextHolder } from '@frontegg/rest-api'
+
 const Login = () => {
   const { user, isAuthenticated } = useAuth();
   const loginWithRedirect = useLoginWithRedirect();
@@ -10,35 +11,42 @@ const Login = () => {
     window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
   };
 
+  if(isAuthenticated){
+    console.log('user: ', user);
+  }
+  else{}
+
   return (
-    <div>
+    <div className='loginPage'>
       {isAuthenticated ? (
         <div>
-          <div>
-            <img src={user?.profilePictureUrl} alt={user?.name} />
-          </div>
-          <div>
-            <span>Logged in as: {user?.name}</span>
-          </div>
+        <div className='isAuthenticated d-flex justify-content-between align-items-center'>
           <div>
             <button onClick={() => alert(user.accessToken)}>
               What is my access token?
             </button>
           </div>
           <div>
+            <button onClick={() => AdminPortal.show()}>Settings</button>
+          </div>
+          <div>
             <button onClick={logoutHandle}>
               Logout
             </button>
           </div>
-          <div>
-            <button onClick={() => AdminPortal.show()}>Settings</button>
-          </div>
         </div>
-      ) : (
+        <div className='cardSection d-flex justify-content-center flex-column align-items-center'>
         <div>
-          <button onClick={()=>loginWithRedirect()}>
-            Click me to login
-          </button>
+          <img src={user?.profilePictureUrl} alt={user?.name} />
+        </div>
+        <div>
+          <span>&nbsp;{user?.name}</span>
+        </div>
+      </div>
+      </div>
+      ) : (
+        <div className='clickToLogin'>
+            <button onClick={()=>loginWithRedirect()}>Click me to login</button>
         </div>
       )}
     </div>
