6516e9add748c101e461af30136773bc454a5268
intelloger-mverma <manish.verma@intelloger.com>
2022-07-06 15:50:35 +0530

    uri conditions

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import '../styles/globals.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { FronteggProvider } from '@frontegg/nextjs'
 
-const development = false;
+const development = true;
 
 const contextOptions = {
-  baseUrl: `${development ? `${process.env.NEXT_PUBLIC_BASE_URL_DEV}` : `${process.env.NEXT_PUBLIC_BASE_URL}`}`,
+  // baseUrl: `${development ? `${process.env.NEXT_PUBLIC_BASE_URL_DEV}` : `${process.env.NEXT_PUBLIC_BASE_URL}`}`,
+  baseUrl: `${process.env.NEXT_PUBLIC_BASE_URL}`,
   clientId: `${process.env.NEXT_PUBLIC_CLIENT_ID}`
   // baseUrl: 'https://app-e9sp8djc4yrt.frontegg.com',
   // clientId: '696c2641-e54b-4b80-bdf9-36e8745a38ee'
